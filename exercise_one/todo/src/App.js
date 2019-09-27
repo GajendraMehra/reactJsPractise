954fbd3f03c2ebfcdec251fb8d15ba50e256e78f
Gajendra Mehra <Gajendramehra14rgnv@gmail.com>
2019-09-27 16:48:37 +0530

    functional component example

---
+++ exercise_one/todo/src/App.js
@@ -0,0 +1,17 @@
+import React  from "react";
+ import  logo  from "./logo.svg";
+
+import  "./App.css";
+function App() {
+  return (  
+    <div className="App">
+    <header className="App-header">
+    <img src={logo} className="App-logo" alt=""/>
+    <h3>HELLO DEAR</h3>
+    </header>
+    <h2>Hello World </h2>
+    </div>
+  
+  );
+}
+export default App;
