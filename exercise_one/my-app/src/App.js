984c8627f2c0d8f7e2e8b9f866a37794d8bf7663
Gajendra Mehra <Gajendramehra14rgnv@gmail.com>
2019-10-06 04:46:53 +0530

    Functional Component Demo

---
+++ exercise_one/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Greet from './components/Greet';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>Hello</h4>
+        <Greet></Greet>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ exercise_one/my-app/src/components/Greet.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// export default function Greet() {
+//     return (
+//         <div>
+//             <h4>Hello from Functional Component</h4>
+//         </div>
+//     )
+// }
+
+const Greet =  ()=> 
+    <h1>Hello</h1>
+
+
+export default Greet
